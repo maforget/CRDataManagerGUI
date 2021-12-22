68fbf83b4eab9ceb7bf89afcac21e2076d574a02
maforget <11904426+maforget@users.noreply.github.com>
2021-12-21 22:59:05 -0500

    Added Icon in RegExEditor

--- csharp/DataManagerGUI/Forms/RegExEditor.designer.cs
+++ csharp/DataManagerGUI/Forms/RegExEditor.designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegExEditor));
             this.textBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -205,6 +206,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RegExEditor";
             this.Text = "RegExEditor";
             this.panel1.ResumeLayout(false);
