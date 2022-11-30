adcd692f3c0be2aa080d83196c45bec83cb11d83
maforget <11904426+maforget@users.noreply.github.com>
2022-11-29 21:27:35 -0500

    GUI 2.07.03.816

--- csharp/DataManagerGUI/Properties/AssemblyInfo.cs
+++ csharp/DataManagerGUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.07.02.814")]
-[assembly: AssemblyFileVersion("2.07.02.814")]
+[assembly: AssemblyVersion("2.07.03.816")]
+[assembly: AssemblyFileVersion("2.07.03.816")]
